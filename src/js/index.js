680d0bed0579aac11acf65ec78883e7c77bfd787
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-20 17:37:44 +0200

    fibonnaci function works for first test case

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+//Number of sequence at index n
+const fibonacci = (n) => {
+  if (n <= 2) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n + 2);
+  }
+};
+console.log(1);
+//Enter a Fibonacci index(n)
+const fibs = (n) => {};
