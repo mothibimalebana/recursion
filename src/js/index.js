67fac084a2255cbe08804b50794dfd97a2ec8011
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-20 18:21:17 +0200

    did a iteration of fibonacci

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
 //Number of sequence at index n
 const fibonacci = (n) => {
-  if (n <= 2) {
+  if (n < 2) {
     return n;
   } else {
-    return fibonacci(n - 1) + fibonacci(n + 2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
 };
-console.log(1);
+console.log(fibonacci(10));
 //Enter a Fibonacci index(n)
-const fibs = (n) => {};
+const fibs = (n) => {
+  let fibsArray = [];
+  for (let i = 0; i <= n; i++) {
+    fibsArray.push(fibonacci(i));
+  }
+  return fibsArray;
+};
+
+console.log(fibs(10));
